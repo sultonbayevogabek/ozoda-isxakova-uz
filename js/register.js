6a695a77363d538f1e7c1f1ffddd86fe24a1226b
sultonbayevogabek <sultonbayevogabek@gmail.com>
2025-06-25 18:30:50 +0500

    Update styles, animations, and dependencies adjustments

--- js/register.js
+++ js/register.js
@@ -6,7 +6,7 @@
 
   document.querySelector('#joinToChannel')?.addEventListener('click', async () => {
     await statistics.onClickTgBtn();
-    window.location.href = 'https://t.me/+2rQsuMRRGGZhMzAy';
+    window.location.href = 'https://t.me/+pQ5RHZUO8A1iMmJi';
   })
 
   if (user && user?.name && user?.phone && user?.time) {
