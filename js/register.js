a93cec412a519eb97ed95dcbb25b166f15b32aab
sultonbayevogabek <sultonbayevogabek@gmail.com>
2025-09-05 15:21:15 +0500

    Update styles, animations, and dependencies adjustments

--- js/register.js
+++ js/register.js
@@ -1,24 +1,19 @@
 'use strict';
 
 (async _ => {
-  // const statistics = new Statistics();
   let user = JSON.parse(localStorage.getItem('user'));
-
   if (user && user?.name && user?.phone && user?.time) {
     const formData = new FormData();
 
     formData.append('Ismi', user?.name);
     formData.append('Telefon raqami', user?.phone);
     formData.append(`Ro'yxatdan o'tgan vaqti`, user?.time);
-    // formData.append(`Foydalanuvchi ID`, statistics.userId);
-    // formData.append(`Timestamp`, statistics.time?.toString());
 
-    let response = await fetch('https://script.google.com/macros/s/AKfycbyDmQnQLuyA2aXzcBQYG4BWmPoCB-sRnU_Q3Iofh5837QpiO83r-lx-rDhieqGkBh9X/exec', {
+    const response = await fetch('https://script.google.com/macros/s/AKfycbyLxxieKEB6Cv8obIpb8shCIApeXVFfqCuV8qf_lYyEzU-v2wVoU1i2S7nbKR7G9YTxMw/exec', {
       method: 'POST',
       body: formData
     })
     await response.json();
-    // await statistics.onRegister(user);
     localStorage.removeItem('user');
   } else {
     localStorage.removeItem('user');
